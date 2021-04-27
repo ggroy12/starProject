f589522966d5755b3799ed4f0ab1a1976ea7bc48
ggroy12 <ggroy12@gmail.com>
2021-04-28 01:55:19 +0300

    Перепись урока. Не окончено. в-2 Lesson 6

--- init-db-statistic.php
+++ init-db-statistic.php
@@ -13,7 +13,7 @@ $pdo->exec('DROP TABLE IF EXISTS battle_history;');
 $pdo->exec(
     'CREATE TABLE `battle_history` (
         `id` int (6) NOT NULL AUTO_INCREMENT,
-        `aWinnerId` int (6),
+        `aWinnerId` int (6) NOT NULL,
         `nameShipId1` int (6) NOT NULL,
         `ship1Quantity` int (6) NOT NULL,
         `remainingStrength1` int (6) NOT NULL,
